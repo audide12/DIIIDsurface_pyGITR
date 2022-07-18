f767e5c345b1c879a3873b3af4a050bed286c754
audide12 <56002782+audide12@users.noreply.github.com>
2022-07-18 16:32:47 -0700

    Add files via upload

---
+++ examples/Workflow_setup/setup_geom_two_plates.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import gmsh
+from pyGITR.Geom import GeomSetup
+
+# Load geometry
+g = GeomSetup('two_plates.msh', Verbose=True)
+
+# Show existing groups
+g.ShowGroups()
+
+# Plot mesh for some groups
+g.Plot()
+
+# Zoom on the plot (the zoom function for 3D axis in matplotlib 3.3.3 is not working so it has to be done manually)
+g.SetAxisLim(-0.02, 0.02)
+
+# Show normals for the boundbox
+g.ShowNormals()
+
+# Set properties for each group
+g.SetElemAttr([],'surface', 1)
+
+
+# set inDir. Empty list = all elements
+g.SetElemAttr([], 'inDir',1)
+
+# Set Z for material
+g.SetElemAttr([], 'Z', 0)
+
+# Set potential for biasing
+g.SetElemAttr([],'potential',0)
+
+# Plot geometry showing values of Z with color
+g.Plot(ElemAttr='Z', Alpha=0.1)
+
+# Write the geometry file
+g.WriteGeomFile(OverWrite=True)
+
+
+
