d2cb9dff10a82e8bdc86e37d214de71d6903de1f
Aritra De <de.aritra1@gmail.com>
2023-04-07 14:58:53 -0700

    OEDGE Background plasma

--- examples/DIMES_2/Input_Zack.py
+++ examples/DIMES_2/Input_Zack.py
@@ -1,118 +1,96 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Sep 20 17:13:10 2022
-
-@author: de
-"""
 
 import os
 from pyGITR import Input
 
-ParticleFile='particleConf.nc'
-GeometryFile='gitrGeom.cfg'
-B0 = 2.25
-thetaB = 2
-phiB = 0
-NP = 10000
+def make_input(nP,dt,nT,ParticleFile='particleConf.nc',GeometryFile='gitrGeom.cfg',folder='../input/'):
 
-# Initiallize input object
-i = Input()
+    B0 = 2.25
+    thetaB = -2
+    phiB = 0
 
-# Add structures to configuration file
-i.SetBField(B0=2.25, theta = thetaB, phi = phiB)
-i.SetTimeStep(dt=1e-9,nT=1e4)
-i.SetGeometryFile(GeometryFile)
-i.SetParticleSource(ParticleFile, nP=NP, Zmax=74, M=183, Z=4)
-i.SetSurfaces()
-i.SetDiagnostics()
-i.SetBackgroundPlasmaProfiles()
-i.SetSurfaceModel()
+    # B0 = -2.25
+    # thetaB = 2
+    # phiB = 0
 
-# i.SetGeomHash()
-# i.Input['flags']['GEOM_HASH'] = 1
-# i.SetGeomSheath()
-# i.Input['flags']['GEOM_HASH_SHEATH'] = 1
+    # Initiallize input object
+    i = Input()
 
-i.Input['backgroundPlasmaProfiles']['Bfield']['interpolation'] = 1 # in fields.cpp, only needs to be >0
-i.Input['flags']['BFIELD_INTERP'] = 2
+    # Add structures to configuration file
+    i.SetBField(B0=B0, theta = thetaB, phi = phiB)
+    i.SetTimeStep(dt=dt,nT=nT)
+    i.SetGeometryFile(GeometryFile)
+    i.SetParticleSource(ParticleFile, nP=nP, Zmax=6, M=12, Z=0)
+    i.SetSurfaces()
+    i.SetDiagnostics()
+    i.SetBackgroundPlasmaProfiles()
+    i.SetSurfaceModel()
+    # i.SetGeomHash()
+    # i.SetGeomSheath()
 
-i.Input['flags']['GENERATE_LC'] = 0
-i.Input['flags']['LC_INTERP'] = 0 # 0, 0 no Lc, 2 get from file, 3 consider flowV
-i.Input['flags']['EFIELD_INTERP'] = 0
-i.Input['flags']['PRESHEATH_INTERP'] = 0 # 3
-i.Input['flags']['DENSITY_INTERP'] = 0 # 3
-i.Input['flags']['TEMP_INTERP'] = 0 # 3
-i.Input['flags']['FLOWV_INTERP'] = 0 # 3, 0 const from file, 1 Lc based, 2 2D, 3 3D
-i.Input['flags']['GRADT_INTERP'] = 0 # 3, 1 R, 2 R+Z, 3 R+Z+Y
-i.Input['flags']['USECYLSYMM'] = 1 # rotates the plasma with cylindrical geometry
+    # Set the standard flags
+    i.Input['flags']['BIASED_SURFACE'] = 0
+    i.Input['flags']['USE_SURFACE_POTENTIAL'] = 0
+    i.Input['flags']['USETHERMALFORCE'] = 1
+    i.Input['flags']['USEPERPDIFFUSION'] = 1
+    i.Input['flags']['USESURFACEMODEL'] = 1
+    i.Input['flags']['USESHEATHEFIELD'] = 1 # 1 or 0, on or off
+    i.Input['flags']['USEPRESHEATHEFIELD'] = 1  # 1 or 0, on or off
+    i.Input['flags']['USECOULOMBCOLLISIONS'] = 1
+    i.Input['flags']['USEFRICTION'] = 1
+    i.Input['flags']['USEANGLESCATTERING'] = 1
+    i.Input['flags']['USEHEATING'] = 1
+    i.Input['flags']['FORCE_EVAL'] = 0
+    i.Input['flags']['USECYLSYMM'] = 1 # rotates the plasma with cylindrical geometry
+    i.Input['flags']['USE3DTETGEOM'] = 0
+    i.Input['flags']['SPECTROSCOPY'] = 2
 
-# i.SetConnectionLength()
-# i.SetForceEvaluation()
+    # Set the INTERP flags
+    i.Input['flags']['BFIELD_INTERP'] = 2
+    i.Input['flags']['EFIELD_INTERP'] = 0
+    i.Input['flags']['PRESHEATH_INTERP'] = 2 # 3
+    i.Input['flags']['DENSITY_INTERP'] = 2
+    i.Input['flags']['TEMP_INTERP'] = 2
+    i.Input['flags']['FLOWV_INTERP'] = 2 # 3, 0 const from file, 1 Lc based, 2 2D, 3 3D
+    i.Input['flags']['GRADT_INTERP'] = 2 # 3, 1 R, 2 R+Z, 3 R+Z+Y
 
-# Set the standard options
-i.Input['flags']['USESURFACEMODEL'] = 0
-i.Input['flags']['USECOULOMBCOLLISIONS'] = 1
-i.Input['flags']['USEFRICTION'] = 1
-i.Input['flags']['USEANGLESCATTERING'] = 1
-i.Input['flags']['USEHEATING'] = 1
-i.Input['impurityParticleSource']['Z'] = 74
-i.Input['impurityParticleSource']['source_material_Z'] = 74
-i.Input['backgroundPlasmaProfiles']['FlowVelocity']['flowVz'] = -2000
+    # Set other options
+    i.Input['backgroundPlasmaProfiles']['Bfield']['interpolation'] = 1 # in fields.cpp, only needs to be >0
+    i.Input['impurityParticleSource']['Z'] = 6
+    i.Input['impurityParticleSource']['source_material_Z'] = 6
+    i.Input['backgroundPlasmaProfiles']['Bfield']['rString'] = 'br'
+    i.Input['backgroundPlasmaProfiles']['Bfield']['zString'] = 'bz'
+    i.Input['backgroundPlasmaProfiles']['Bfield']['yString'] = 'bt'
+    i.Input['backgroundPlasmaProfiles']['Diffusion']['Dperp'] = 0.1
+    i.Input['surfaceModel']['fileString'] = 'surface_model_C-C.nc'
+    i.Input['backgroundPlasmaProfiles']['FlowVelocity']['flowVr'] = 20000
 
-i.Input['backgroundPlasmaProfiles']['Bfield']['rString'] = 'br'
-i.Input['backgroundPlasmaProfiles']['Bfield']['zString'] = 'bz'
-i.Input['backgroundPlasmaProfiles']['Bfield']['yString'] = 'bt'
-
-# Write input file
-i.WriteInputFile(Folder='input',OverWrite=True)
-
-
-#%%
-
-FileNameHistory='/Users/de/Research/DIIIDsurface_pyGITR/examples/DIMES/output/history.nc'
-HistoryData = Dataset(FileNameHistory, "r", format="NETCDF4")
-x = np.array(HistoryData.variables['x'])
-z = np.array(HistoryData.variables['z'])
-y = np.array(HistoryData.variables['y'])
-
-nT = HistoryData.dimensions['nT'].size
-nP = HistoryData.dimensions['nP'].size
-from mpl_toolkits.mplot3d import Axes3D
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-for i in range(0,nP):
-    ax.plot(x[i,:],y[i,:],z[i,:])
-g.Plot(ElemAttr='Z', Alpha=0.1, fig=fig, ax=ax)    
+    i.Input['impurityParticleSource']['ionization']['fileString'] = 'ADAS_Rates_C.nc'
+    i.Input['impurityParticleSource']['recombination']['fileString'] = 'ADAS_Rates_C.nc'
 
 
 
-#%%
-#Reading geometry files
+    i.Input['surfaces']['flux']['nE'] = 200
+    i.Input['surfaces']['flux']['E0'] = 0
+    i.Input['surfaces']['flux']['E'] = 200
+    i.Input['surfaces']['flux']['nA'] = 90
+    i.Input['surfaces']['flux']['A0'] = 0
+    i.Input['surfaces']['flux']['A'] = 90
 
-filename="input/gitrGeom.cfg"
+    i.Input['diagnostics']['trackSubSampleFactor'] = 10
+    i.Input['diagnostics']['netx0'] = 1.38
+    i.Input['diagnostics']['netx1'] = 1.58
+    i.Input['diagnostics']['nX'] = 250
+    i.Input['diagnostics']['nety0'] = -0.1
+    i.Input['diagnostics']['nety1'] = 0.1
+    i.Input['diagnostics']['nY'] = 250
+    i.Input['diagnostics']['netz0'] = -1.25
+    i.Input['diagnostics']['netz1'] = -1.05
+    i.Input['diagnostics']['nZ'] = 250
+    i.Input['diagnostics']['densityChargeBins'] = 6
 
-with io.open(filename) as f:
-    config = libconf.load(f)
+    # Write input file
+    i.WriteInputFile(Folder=folder,OverWrite=True)
 
-x1 = np.array(config.geom.x1)
-x2 = np.array(config.geom.x2)
-x3 = np.array(config.geom.x3)
-y1 = np.array(config.geom.y1)
-y2 = np.array(config.geom.y2)
-y3 = np.array(config.geom.y3)
-z1 = np.array(config.geom.z1)
-z2 = np.array(config.geom.z2)
-z3 = np.array(config.geom.z3)
-area = np.array(config.geom.area)
-surf = np.array(config.geom.surface)
-Z = np.array(config.geom.Z)
-materialSurfaceInidces = np.nonzero(Z)
-surfIndArray = np.asarray(materialSurfaceInidces)
-a = np.array(config.geom.a)
-b = np.array(config.geom.b)
-c = np.array(config.geom.c)
-d = np.array(config.geom.d)
-
-in_direction = np.array(config.geom.inDir)
-plane_norm = np.array(config.geom.plane_norm)
+if __name__ == '__main__':
+    make_input(1000)
---
+++ examples/MATLAB_DRIVE/mine/reading_netcdf.m
@@ -0,0 +1,6 @@
+ADAS_W = ncread('ADAS_Rates_W.nc')
+
+ADAS_C = ncread('ADAS_Rates_C.nc')
+
+
+
